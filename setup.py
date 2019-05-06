5ab2e77808bf0c46ea4a1ea3c73d138d65847e7d
Ivan Grunev <ivan.grunev@gmail.com>
2019-05-06 18:42:28 +0300

    refs #1. Update version

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 from pip._internal.req import parse_requirements
-import os
 
 install_reqs = parse_requirements("requirements.txt", session="hack")
 
-version = "0.0.2"
+version = "0.0.3"
 
 setup(
     name="pydatalake-gen2",
