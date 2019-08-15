3263a501fe452e1835c3b743cdd04f22e78513f4
Ivan Grunev <ivan.grunev@gmail.com>
2019-08-15 14:26:15 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from pip._internal.req import parse_requirements
 
 install_reqs = parse_requirements("requirements.txt", session="hack")
 
-with open("README.md") as f:
+with open("README.md", "r") as f:
     README = f.read()
 
-version = "0.0.5"
+version = "0.0.6"
 
 setup(
     name="pydatalake-gen2",
@@ -15,17 +15,15 @@ setup(
     author_email="ivan.grunev@gmail.com",
     include_package_data=True,
     long_description=README,
+    long_description_content_type='text/markdown',
     python_requires=">=3.4",
     version=version,
     url="https://github.com/estatic/pydatalake-gen2",
     packages=find_packages(),
     zip_safe=False,
     classifiers=[
-        "Development Status :: 3 — Alpha",
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
